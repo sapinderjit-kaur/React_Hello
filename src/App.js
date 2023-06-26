3c4ce0ea93467fefcbf169e852b647a36bda13b4
Sapinderjit <er.sapinder.cs@gmail.com>
2023-06-26 14:26:16 -0400

    Dep second

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to first React Application</h1>
+      <h1>Welcome to first React App</h1>
     </div>
   );
 }
